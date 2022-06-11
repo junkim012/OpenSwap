9b2b6a0ad00df5ad674a68c60ddafe6d2838f444
Yevhenii Rachkovan <yevheniirachkovan@Yevheniis-MacBook-Pro.local>
2022-06-11 19:08:46 -0400

    Kek

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,9 +1,32 @@
-import { useState } from "react/cjs/react.production.min";
+import React, { useEffect, useState } from 'react';
 
 function Marketplace() {
     const [assets, setAssets] = useState([])
+    useEffect(() => {
+        setAssets([
+            { "img": "", "name": "Nanachi", "id": 1233 },
+            { "img": "", "name": "Nanachi 2", "id": 12332 }
+        ])
+    }, [])
     return (
-        <p>Nanachi</p>
+        assets.map((asset) => {
+            return (
+                <div key={asset.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"></img>
+                    <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">{asset.name}</div>
+                        <p className="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                    <div className="px-6 pt-4 pb-2">
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                    </div>
+                </div>
+            )
+        })
     )
 }
 
